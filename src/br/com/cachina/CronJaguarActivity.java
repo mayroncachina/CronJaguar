1ee1a138c2cb59ef8eb4173ad167a9fa352d0a5c
Mayron Cachina <mayroncachina@gmail.com>
2011-10-18 21:38:26 -0300

    Fix Project

--- src/br/com/cachina/CronJaguarActivity.java
+++ src/br/com/cachina/CronJaguarActivity.java
@@ -1,12 +1,12 @@
 package br.com.cachina;
 
-import com.phonegap.DroidGap;
-
 import android.os.Bundle;
 
-public class HelloWorldActivity extends DroidGap {
+import com.phonegap.DroidGap;
+
+public class CronJaguarActivity extends DroidGap {
     /** Called when the activity is first created. */
-    
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.loadUrl("file:///android_asset/www/index.html");
